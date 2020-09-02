a178494c12fe48c109856abe676560f018be83db
Ed Howard <edward.x.howard@veeam.com>
2020-09-02 16:53:24 +0100

    Change to created vs modified

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # filescanner
 
-First attempt at a method of scanning a filesystem and outputting the relationship between the created date and the days since it was last modified. 
+First attempt at a method of scanning a filesystem and outputting the relationship between the created date and modified date.
 
-The point of this is to show if a given filesystem is being modified over time or if data is not being modified.
+The point of this is to show if a given filesystem is being modified over time.
 
 You will need Python as well as the following packages:
 
--- filescanner.py
+++ filescanner.py
@@ -4,23 +4,12 @@ import PySimpleGUI as sg
 from pathlib import Path
 
 sg.theme('Dark Blue 3')
-
 path = sg.popup_get_folder('Please select folder')
-
 df = folderstats.folderstats(path, ignore_hidden=True)
-
 df['Size GB'] = round(df['size'] / (1024 ** 2),2)
-
 df = df[['ctime', 'mtime', 'size', 'Size GB']]
-df['Time'] = df['mtime'] - df['ctime']
-df['days'] = df['Time'].dt.days
-df.loc[df['days'] > 0, 'days2'] = df['days']
-df.loc[df['days'] < 0, 'days2'] = 0
-# results = df.groupby(['ctime', 'mtime']).sum()
-
-plt.plot_date(df['ctime'], df['days2'])
-
-# results.to_excel('file_data_info.xlsx')
+plt.plot_date(df['ctime'], df['mtime'])
+plt.ylabel('Modified Date')
+plt.xlabel('Created Date')
 sg.popup('All done!')
-
 plt.show()
